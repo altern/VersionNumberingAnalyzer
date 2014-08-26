d591299e75e881ae0cc66b280022592fe550b0e7
altern <sergey.shmarkatyuk@gmail.com>
2014-08-26 07:12:42 -0700

    refactored version numbering analyzer

--- lib/version_numbering_analyzer.rb
+++ lib/version_numbering_analyzer.rb
@@ -1,7 +1,6 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-require_relative 'release_histories'
 require 'version_inconsistencies'
 require 'version_number'
 require 'utils'
@@ -18,132 +17,94 @@ require 'utils'
 
 class VersionNumberingAnalyzer
   
-  attr_accessor :project, :version, :releaseHistory, :releaseHistories, :versionPattern
+  attr_accessor :releaseHistory, :versionInconsistencies
   
-  def initialize(project = nil)
-    @versionPattern = /^(\D*)?(\d+)([\._\s]([\dx]+))?([\._]([\dx]+))?([\._]([\dx]+))?([\._]([\dx]+))?(([-_\.\s]?(([a-zA-Z]*)[-_\s]?(\d*)))?(.*)?)?$/
-    @releaseHistories = ReleaseHistories.new.releaseHistories
-    @project = project
-  end
-  
-  @@versionCompoundMethods = {
-    :fullVersion => 0,
-    :prefix => 1,
-    :firstVersionCompound => 2,
-    :firstVersionCompoundWithSeparator => 3,
-    :secondVersionCompound => 4,
-    :secondVersionCompoundWithSeparator => 5,
-    :thirdVersionCompound => 6,
-    :thirdVersionCompoundWithSeparator => 7,
-    :fourthVersionCompound => 8,
-    :x1 => 9,
-    :x2 => 10,
-    :x3 => 11,
-    :fullSuffix => 12,
-    :suffixLabelWithNumber => 13,
-    :suffixLabel => 14,
-    :suffixNumber => 15,
-    :postSuffix => 16,
-  }
-  
-  def versionCompoundMethods 
-    @@versionCompoundMethods 
-  end
-  
-  def self.versionCompoundMethods 
-    @@versionCompoundMethods 
-  end
-  
-  @@stats = {
-    :parsedVersions => Proc.new {|arr| arr.find_all{ |x| !x.nil? } },
-  }
-  @@metrics = {
-    :numberOfUniqueValues => Proc.new {|arr| arr.uniq.length },
-    :uniqueValues => Proc.new {|arr| arr.uniq},
-    :numberOfEmptyValues => Proc.new {|arr| arr.find_all{|i| i == ''}.length},
-    :distributionOfUniqueValues => Proc.new {|arr| arr.uniq.map { |uniq| 
-      { uniq => ((arr.find_all{ |i| i == uniq}.length.to_f/arr.length*100).round(2)).to_s + "%" }
-    }.reduce Hash.new, :merge },
-  }
-  
-  def getIncrementMetrics
-    vi = VersionInconsistencies.new
-    @@versionCompoundMethods.each { |key, compoundId|
-      compound = getCompoundById(compoundId)
-      compound.each_with_index { |value, i|
+  def initialize(releaseHistory = nil)
+    @releaseHistory = releaseHistory
+    @versionInconsistencies = VersionInconsistencies.new
+    if !@releaseHistory.nil? then 
+      @releaseHistory.each_with_index{ |strVersion, i|
         if i != 0
-          curr = try_to_i(compound[i])
-#          version2 = getCompounds(i).fullVersion
-          if curr == 'x'
-            vi.incrementVersionPlaceholder
-          end
-          prev = try_to_i(compound[i-1])
-#          version1 = getCompounds(i-1).fullVersion
-          if curr.nil? ^ prev.nil?
-            vi.incrementEmptyJump(compoundId)
-          elsif !curr.nil? && !prev.nil?
-            if (curr - prev == 1) then
-              vi.incrementVersionCompound(compoundId)
-            elsif (curr - prev >= 2 )
-              vi.incrementJump(compoundId)
-#              vi.addJumpPair(compoundId, version1, version2)
-            elsif curr < prev 
-              if curr > 0
-                vi.incrementJump(compoundId)
+          strVersion1 = @releaseHistory[i-1]
+          strVersion2 = @releaseHistory[i]
+          version1 = VersionNumber.new(strVersion1)
+          version2 = VersionNumber.new(strVersion2)
+          
+          VersionNumber.numericCompounds.each { |key, value|
+            if key.class == Symbol
+              compoundId = VersionNumber.versionCompoundMethods[key]
+            else 
+              compoundId = key
+            end
+          
+            compound1 = version1.getCompoundById(compoundId)
+            compound2 = version2.getCompoundById(compoundId)
+            if compound2 == 'x'
+              @versionInconsistencies.incrementVersionPlaceholder(compoundId)
+            end
+            if compound2.nil? ^ compound1.nil?
+              @versionInconsistencies.incrementEmptyJump(compoundId)
+            elsif compound2.class == Fixnum && compound1.class == Fixnum
+              if (compound2 - compound1 == 1) then
+                @versionInconsistencies.incrementVersionCompound(compoundId)
+              elsif ( compound2 - compound1 >= 2 ) && (!compound1.nil? && !compound2.nil?)
+                @versionInconsistencies.incrementJump(compoundId)
+                @versionInconsistencies.addJumpPair(compoundId, strVersion1, strVersion2)
+                if compoundId == 2 
+                  puts "adding jump pair (#{compoundId}): #{strVersion1}, #{strVersion2}"
+                end 
+              elsif compound2 < compound1 
+                if compound2 > 0
+                  @versionInconsistencies.incrementJump(compoundId)
+                end
               end
             end
-          end
-        end
-      }
-    }
-    if !@project.nil?
-      releaseHistory = @releaseHistories[@project]
-      releaseHistory.each_with_index { |value,i| 
-        if i != 0 
-          version1 = releaseHistory[i-1]
-          version2 = releaseHistory[i]
+          }
           
-          severity = vi.versionMegalomaniaSeverity(version1, version2)
+          severity = @versionInconsistencies.versionMegalomaniaSeverity(version1, version2)
           if (severity != 0)
-            vi.addMegalomaniaSeverity(severity)
-            vi.addMegalomaniaSeverityPair(version1, version2)
-          end
-          
-#          parsedVersion1 = Version.new(version1)
-#          parsedVersion2 = Version.new(version2)
-#          
-        end
-      }
-    elsif !@releaseHistory.nil? 
-      @releaseHistory.each_with_index { |value,i| 
-        if i != 0 
-          version1 = @releaseHistory[i-1]
-          version2 = @releaseHistory[i]
-          severity = vi.versionMegalomaniaSeverity(VersionNumber.new(version1), VersionNumber.new(version2))
-          if (severity != 0)
-            vi.addMegalomaniaSeverity(severity)
-            vi.addMegalomaniaSeverityPair(version1, version2)
+            @versionInconsistencies.addMegalomaniaSeverity(severity)
+            @versionInconsistencies.addMegalomaniaSeverityPair(strVersion1, strVersion2)
           end
         end
       }
     end
-    vi
   end
   
+  @@stats = {
+    :parsedVersions => Proc.new {|strVersionList| strVersionList.find_all{ |x| !x.nil? } },
+  }
+  @@metrics = {
+    :numberOfUniqueValues => Proc.new {|strVersionList, method| 
+      strVersionList.map{|strVersion| 
+        VersionNumber.new(strVersion).getCompoundById(method)
+      }.uniq.length 
+    },
+    :uniqueValues => Proc.new {|strVersionList, method| 
+      strVersionList.map{|strVersion| 
+        VersionNumber.new(strVersion).getCompoundById(method)
+      }.uniq
+    },
+    :numberOfEmptyValues => Proc.new {|strVersionList, method| 
+      strVersionList.map{|strVersion| 
+        VersionNumber.new(strVersion).getCompoundById(method)
+      }.find_all{|i| i == ''}.length
+    },
+    :distributionOfUniqueValues => Proc.new {|strVersionList, method| 
+      strVersionList.map{|strVersion| 
+        VersionNumber.new(strVersion).getCompoundById(method)
+      }.uniq.map { |uniq| 
+        { uniq => ((strVersionList.find_all{ |i| i == uniq }.length.to_f/strVersionList.length*100).round(2)).to_s + "%" }
+    }.reduce Hash.new, :merge },
+  }
+  
+  
   def getMegalomaniaSeverities
-    getIncrementMetrics.megalomaniaSeverities
+    @versionInconsistencies.megalomaniaSeverities
   end
   
   def getMegalomaniaSeverityPairs
-    getIncrementMetrics.megalomaniaSeverityPairs
-  end
-  
-  def version(version = nil) 
-    @version = version
-  end
-  
-  def_each @@versionCompoundMethods.keys do |method_name|
-    getCompoundById(@@versionCompoundMethods[method_name])
+    @versionInconsistencies.megalomaniaSeverityPairs
   end
   
   def_each @@stats.keys do |method_name|
@@ -153,62 +114,11 @@ class VersionNumberingAnalyzer
   end
   
   def_each @@metrics.keys do |metric_method|
-    @@versionCompoundMethods.map { |method_name,id| 
-#      method_name.to_s + " => " + @@metrics[metric_method].call(getCompounds.map{|row| row[id]}).to_s
-      @@metrics[metric_method].call(getCompounds.map{|row| row[id] unless row.nil?})
-    }
-  end
-  
-  def getCompounds 
-    if !@project.nil?
-      @releaseHistories[@project].map { |item| 
-        matches = @versionPattern.match(item) 
-        if !matches.nil? then
-          @@versionCompoundMethods.map{|method,id| matches[id]}
-        end
+    if !@releaseHistory.nil? 
+      VersionNumber.versionCompoundMethods.map { |method_name,id| 
+        @@metrics[metric_method].call(@releaseHistory, method_name)
       }
-    elsif !@releaseHistory.nil? 
-      @releaseHistory.map { |item| 
-        matches = @versionPattern.match(item) 
-        if !matches.nil? then
-          @@versionCompoundMethods.map{|method,id| matches[id]}
-        end
-      }
-    elsif !@version.nil?
-      @versionPattern.match(@version)
     end 
-  end
-  
-  def getCompoundById id
-    if !@version.nil? then
-      matches = @versionPattern.match(@version) 
-      if !matches.nil? then
-        matches[id]
-      end
-    elsif !@project.nil?
-      @releaseHistories[@project].map { |item| 
-        matches = @versionPattern.match(item) 
-        if !matches.nil?
-          matches[id]
-        end
-      }
-    elsif !@releaseHistory.nil? 
-      @releaseHistory.map { |version|
-        matches = @versionPattern.match(version)
-        if !matches.nil? 
-          matches[id]
-        end
-      }
-    else
-      @releaseHistories.map { |key, versions| 
-        versions.map { |version|
-          matches = @versionPattern.match(version)
-          if !matches.nil? 
-            matches[id]
-          end
-        } 
-      }.flatten
-    end
   end
   
 end
--- test/version_numbering_analyzer_test.rb
+++ test/version_numbering_analyzer_test.rb
@@ -11,8 +11,7 @@ require 'utils'
 class VersionNumberingAnalyzerTest < Test::Unit::TestCase
   
   def setup
-    @vna = VersionNumberingAnalyzer.new
-    @vna.releaseHistory = Utils.readFileToArray('test/git_release_history.txt')
+    @vna = VersionNumberingAnalyzer.new(Utils.readFileToArray('test/git_release_history.txt'))
     @number_of_release_history_entiries = 486
   end
   
@@ -25,80 +24,97 @@ class VersionNumberingAnalyzerTest < Test::Unit::TestCase
   end
   
   def test_number_of_unique_values
-    assert_equal(3, @vna.uniqueValues[@vna.versionCompoundMethods[:firstVersionCompound]].length)
-    assert_equal(11, @vna.uniqueValues[@vna.versionCompoundMethods[:secondVersionCompound]].length)
-    assert_equal(15, @vna.uniqueValues[@vna.versionCompoundMethods[:thirdVersionCompound]].length)
-    assert_equal(10, @vna.uniqueValues[@vna.versionCompoundMethods[:fourthVersionCompound]].length)
-    assert_equal(16, @vna.uniqueValues[@vna.versionCompoundMethods[:suffixLabel]].length)
-    assert_equal(9, @vna.uniqueValues[@vna.versionCompoundMethods[:suffixNumber]].length)
+    assert_equal(3, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:firstVersionCompound]].length, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(11, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:secondVersionCompound]].length)
+    assert_equal(15, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:thirdVersionCompound]].length)
+    assert_equal(10, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:fourthVersionCompound]].length)
+    assert_equal(16, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:suffixLabel]].length)
+    assert_equal(9, @vna.uniqueValues[VersionNumber.versionCompoundMethods[:suffixNumber]].length)
   end
   
   def test_increments
-    @vna.releaseHistory = ['0.1','0.2','0.3']
-    assert_equal(0, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(2, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0','1.1','1.2', '1.3', '2.0', '2.1']
-    assert_equal(1, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(4, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1']
-    assert_equal(1, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(1, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(3, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:thirdVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2']
-    assert_equal(1, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(3, @vna.getIncrementMetrics.increments[@vna.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['0.1','0.2','0.3'])
+    assert_equal(0, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:firstVersionCompound]], @vna.versionInconsistencies.increments)
+    assert_equal(2, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:secondVersionCompound]], @vna.versionInconsistencies.increments)
+    @vna = VersionNumberingAnalyzer.new(['1.0','1.1','1.2', '1.3', '2.0', '2.1'])
+    assert_equal(1, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(4, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1'])
+    assert_equal(1, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(1, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal(3, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2'])
+    assert_equal(1, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal(3, @vna.versionInconsistencies.increments[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
   end
   
   def test_empty_jumps
-    @vna.releaseHistory = ['0.1','0.2','0.3']
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0','1.1','1.2', '1.3', '2.0', '2.1']
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1']
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(1, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:thirdVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2']
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.emptyJumps[@vna.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['0.1','0.2','0.3'])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0','1.1','1.2', '1.3', '2.0', '2.1'])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1'])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+#    assert_equal(1, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2'])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.emptyJumps[VersionNumber.versionCompoundMethods[:thirdVersionCompound]], @vna.versionInconsistencies.emptyJumps)
   end
   
   def test_jumps
-    @vna.releaseHistory = ['0.1','0.2','0.3']
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0','1.1','1.2', '1.3', '2.0', '2.1']
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1']
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:thirdVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2']
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:firstVersionCompound]])
-    assert_equal(0, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:secondVersionCompound]])
-    assert_equal(2, @vna.getIncrementMetrics.jumps[@vna.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['0.1','0.2','0.3'])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0','1.1','1.2', '1.3', '2.0', '2.1'])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1'])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2'])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal(0, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal(2, @vna.versionInconsistencies.jumps[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+  end
+  
+  def test_jump_pairs
+    @vna = VersionNumberingAnalyzer.new(['0.1','0.2','0.3'])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0','1.1','1.2', '1.3', '2.0', '2.1'])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1'])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2'])
+#    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:firstVersionCompound]], @vna.versionInconsistencies.jumpPairs)
+#    assert_equal([], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    assert_equal([['1.0.1','1.0.3']], @vna.versionInconsistencies.jumpPairs[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
   end
   
   def test_cycle_lengths
-    @vna.releaseHistory = ['0.1','0.2','0.3']
-    assert_equal([], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:firstVersionCompound]])
-#    assert_equal([2], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0','1.1','1.2', '1.3', '2.0', '2.1']
-#    assert_equal([1], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:firstVersionCompound]])
-#    assert_equal([3,1], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:secondVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1']
-#    assert_equal([1], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:firstVersionCompound]])
-#    assert_equal([1], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:secondVersionCompound]])
-#    assert_equal([3], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:thirdVersionCompound]])
-    @vna.releaseHistory = ['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2']
-#    assert_equal([1], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:firstVersionCompound]])
-#    assert_equal([0], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:secondVersionCompound]])
-#    assert_equal([3,2], @vna.getIncrementMetrics.cycleLengths[@vna.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['0.1','0.2','0.3'])
+    assert_equal([], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+#    assert_equal([2], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0','1.1','1.2', '1.3', '2.0', '2.1'])
+#    assert_equal([1], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+#    assert_equal([3,1], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.2', '1.0.3', '2.0', '2.1'])
+#    assert_equal([1], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+#    assert_equal([1], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+#    assert_equal([3], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
+    @vna = VersionNumberingAnalyzer.new(['1.0.0','1.0.1','1.0.3', '1.0.4', '2.0.1', '2.0.2'])
+#    assert_equal([1], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:firstVersionCompound]])
+#    assert_equal([0], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:secondVersionCompound]])
+#    assert_equal([3,2], @vna.versionInconsistencies.cycleLengths[VersionNumber.versionCompoundMethods[:thirdVersionCompound]])
   end
   
 end
