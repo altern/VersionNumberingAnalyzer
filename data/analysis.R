c0582d1a783ae08ac337f2543d10abed5875974b
altern <sergey.shmarkatyuk@gmail.com>
2014-09-03 14:42:17 -0700

    committing results of data analysis

--- data/analysis.R
+++ data/analysis.R
@@ -1,64 +1,282 @@
 source('util.R')
 
-calculateAggregatedMetrics <- function(d1, d2) {
-	print("    Aggregated jumps:")
-	v1 <- as.numeric(d1$Aggregated.jumps)
-	v2 <- as.numeric(d2$Aggregated.jumps)
-	print(paste0("        OSS projects average: ", mean(v1)))
-	print(paste0("        Proprietary projects average: ", mean(v2)))
-	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
-	print(paste0("    p-value = ", pvalue))
-	print("    Aggregated empty jumps:")
-	v1 <- as.numeric(d1$Aggregated.empty.jumps)
-	v2 <- as.numeric(d2$Aggregated.empty.jumps)
-	print(paste0("        OSS projects average: ", mean(v1)))
-	print(paste0("        Proprietary projects average: ", mean(v2)))
-	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
-	print(paste0("    p-value = ", pvalue))
-	print("    Aggregated megalomania severity:")
-	v1 <- as.numeric(d1$Aggregated.megalomania.severity)
-	v2 <- as.numeric(d2$Aggregated.megalomania.severity)
-	print(paste0("        OSS projects average: ", mean(v1)))
-	print(paste0("        Proprietary projects average: ", mean(v2)))
-	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
-	print(paste0("    p-value = ", pvalue))
-	print("    Aggregated cycle lengths:")
-	v1 <- as.numeric(d1$Aggregated.cycle.length)
-	v2 <- as.numeric(d2$Aggregated.cycle.length)
-	print(paste0("        OSS projects average: ", mean(v1)))
-	print(paste0("        Proprietary projects average: ", mean(v2)))
-	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
-	print(paste0("    p-value = ", pvalue))
-	print("    Aggregated version placeholders:")
-	v1 <- as.numeric(d1$Aggregated.version.placeholders)
-	v2 <- as.numeric(d2$Aggregated.version.placeholders)
-	print(paste0("        OSS projects average: ", mean(v1)))
-	print(paste0("        Proprietary projects average: ", mean(v2)))
-	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
-	print(paste0("    p-value = ", pvalue))
-
-
+printTestResults <- function(v1, v2, name1, name2) {
+  print(paste0("        ", name1, " average: ", mean(v1)))
+  print(paste0("        ", name2, " average: ", mean(v2)))
+  pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
+  if (pvalue < 0.05) {
+    print(paste0("    !!! wilcox p-value = ", pvalue, " !!!"))
+  } else {
+    print(paste0("    wilcox p-value = ", pvalue))
+  }
+  return(pvalue)
+#   pvalue <- chisq.test(v1, v2)$p.value
+#   print(paste0("    chisq p-value = ", pvalue))
 }
-data <- read.csv('results.csv', header=TRUE, stringsAsFactors=FALSE)
+
+calculateAggregatedMetrics <- function(d1, d2, name1, name2) {
+  print("Size of samples:")
+  print(paste0("    ", name1, ": ", nrow(d1)))
+  print(paste0("    ", name2, ": ", nrow(d2)))
+  print("")
+
+  metricNames <- c()
+  averages1 <- c()
+  averages2 <- c()
+  names1 <- c()
+  names2 <- c()
+  pvalues <- c()
+  
+  print("== 3rd version compound metrics == ")
+  metricName <- "3rd version compound number of jumps"
+  print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_number_of_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_number_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "3rd version compound sum of jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_sum_of_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_sum_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "3rd version compound avg of jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_avg_of_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_avg_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  metricName <- "3rd version compound number of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_number_of_empty_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_number_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "3rd version compound sum of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_sum_of_empty_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_sum_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "3rd version compound avg of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X3rd_vc_avg_of_empty_jumps)
+  v2 <- as.numeric(d2$X3rd_vc_avg_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+
+  print("== 4th version compound metrics == ")
+  metricName <- "4th version compound number of jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_number_of_jumps)
+  v2 <- as.numeric(d2$X4th_vc_number_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "4th version compound sum of jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_sum_of_jumps)
+  v2 <- as.numeric(d2$X4th_vc_sum_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "4th version compound avg of jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_avg_of_jumps)
+  v2 <- as.numeric(d2$X4th_vc_avg_of_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "4th version compound number of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_number_of_empty_jumps)
+  v2 <- as.numeric(d2$X4th_vc_number_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "4th version compound sum of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_sum_of_empty_jumps)
+  v2 <- as.numeric(d2$X4th_vc_sum_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  metricName <- "4th version compound avg of empty jumps"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$X4th_vc_avg_of_empty_jumps)
+  v2 <- as.numeric(d2$X4th_vc_avg_of_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  print("== Aggregated metrics == ")
+  metricName <- "Aggregated cycle lengths"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$Aggregated_cycle_length)
+  v2 <- as.numeric(d2$Aggregated_cycle_length)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+	metricName <- "Aggregated jumps"; print(paste0("    ", metricName,":"))
+	v1 <- as.numeric(d1$Aggregated_jumps)
+	v2 <- as.numeric(d2$Aggregated_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+	metricName <- "Aggregated empty jumps"; print(paste0("    ", metricName,":"))
+	v1 <- as.numeric(d1$Aggregated_empty_jumps)
+	v2 <- as.numeric(d2$Aggregated_empty_jumps)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  metricName <- "Aggregated sum of megalomania severities"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$Sum_of_megalomania_severities)
+  v2 <- as.numeric(d2$Sum_of_megalomania_severities)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  metricName <- "Aggregated number of megalomania severities"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$Number_of_megalomania_severities)
+  v2 <- as.numeric(d2$Number_of_megalomania_severities)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  metricName <- "Aggregated average of megalomania severities"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$Average_of_megalomania_severities)
+  v2 <- as.numeric(d2$Average_of_megalomania_severities)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  
+  metricName <- "Aggregated inconsistency score"; print(paste0("    ", metricName,":"))
+  v1 <- as.numeric(d1$Aggregated_inconsistency_score)
+  v2 <- as.numeric(d2$Aggregated_inconsistency_score)
+  pvalue <- printTestResults(v1, v2, name1, name2)
+  if(pvalue < 0.05) {
+    metricNames <- c(metricNames, metricName); 
+    averages1 <- c(averages1, mean(v1)); averages2 <- c(averages2, mean(v2))
+    names1 <- c(names1, name1); names2 <- c(names2, name2)
+    pvalues <- c(pvalues, pvalue)
+  }
+  analysis_results <- data.frame(metricNames, averages1, averages2, pvalues)
+  if(nrow(analysis_results) != 0) {
+    names(analysis_results) <- c("Metric names", paste0(name1, " averages"),paste0(name2, " averages"), "p-values")  
+  }
+  
+  return(analysis_results)
+# 	metricName <- "Aggregated version placeholders"
+# 	v1 <- as.numeric(d1$Aggregated.version.placeholders)
+# 	v2 <- as.numeric(d2$Aggregated.version.placeholders)
+# 	print(paste0("        ", name1, " average: ", mean(v1)))
+# 	print(paste0("        ", name2, " average: ", mean(v2)))
+# 	pvalue <- wilcox.test(v1, v2, paired=FALSE, exact=FALSE)$p.value
+# 	print(paste0("    p-value = ", pvalue))
+}
+results <- read.csv('results.csv', header=TRUE, stringsAsFactors=FALSE)
+data = sqldf("select * from results group by Project_name having Number_of_versions == MAX(Number_of_versions)")
 columns <- colnames(data)
 
-print("OSS vs Proprietary wilcox ")
-ossProjects <- data[data$Source.type == "OSS", ]
-proprietaryProjects <- data[data$Source.type == "Proprietary", ]
-calculateAggregatedMetrics(ossProjects, proprietaryProjects)
+print("OSS vs Proprietary ")
+ossProjects <- data[data$Source_type == "OSS", ]
+proprietaryProjects <- data[data$Source_type == "Proprietary", ]
+analysis_results <- calculateAggregatedMetrics(ossProjects, proprietaryProjects, "OSS projects", "Proprietary projects")
+print(analysis_results)
+write.csv(analysis_results, "oss_analysis_results.csv")
+print("")
 
-print("SVN vs Git wilcox ")
+print("SVN vs Git ")
 projectsFromSvn <- data[data$Source == "svn", ]
 projectsFromGit <- data[data$Source == "git", ]
-calculateAggregatedMetrics(projectsFromSvn, projectsFromGit)
+analysis_results <- calculateAggregatedMetrics(projectsFromSvn, projectsFromGit, "Projects from SVN", "Projects from Git")
+print(analysis_results)
+write.csv(analysis_results, "vcs_analysis_results.csv")
+print("")
 
-print("DevTools vs non-dev tools wilcox ")
-devTools <- data[data$Application.type == "DevTool", ]
-notDevTools <- data[data$Application.type != "DevTool", ]
-calculateAggregatedMetrics(devTools,notDevTools)
+print("DevTools vs non-dev tools ")
+devTools <- data[data$Application_type == "DevTool", ]
+notDevTools <- data[data$Application_type != "DevTool", ]
+analysis_results <- calculateAggregatedMetrics(devTools,notDevTools, "Development tools", "Non-development tools")
+print(analysis_results)
+write.csv(analysis_results, "devtools_analysis_results.csv")
+print("")
 
-print("SDK projects vs non-SDK projects wilcox ")
-sdkProjects <- data[data$Development.toolset != "--", ]
-notSdkProjects <- data[data$Development.toolset == "--", ]
-calculateAggregatedMetrics(sdkProjects,notSdkProjects)
+print("SDK projects vs non-SDK projects ")
+sdkProjects <- data[data$Development_toolset != "--", ]
+notSdkProjects <- data[data$Development_toolset == "--", ]
+analysis_results <- calculateAggregatedMetrics(sdkProjects,notSdkProjects, "SDK projects", "Non-SDK projects")
+print(analysis_results)
+write.csv(analysis_results, "sdk_analysis_results.csv")
+print("")
 
